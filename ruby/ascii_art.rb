2151a05293e1e1ff77d73e43ab6dcb4afe544b06
supermomonga <hi@supermomonga.com>
2015-01-10 05:14:57 +0900

    remove _ definition

--- ruby/ascii_art.rb
+++ ruby/ascii_art.rb
@@ -1,20 +1,17 @@
 __,__,f,i,z,z,__,__,b,u,z,z,__,__=DATA.map(&method(:eval)).map &:size
-_=([_]*i).map(&:to_i).inject &:** if !_
-
-F        = (f* i+z+z+b+u+z+z) *(i**i*i-_)/i
-  I      = (f**i+z+z+b+u+z+z) +i**i+_
+F        = (f* i+z+z+b+u+z+z) *(i**i*i-i**z)/i
+  I      = (f**i+z+z+b+u+z+z) +i**i+i**z
     Z    = (f**i+z+z+b+u+z+z) +f*i+i
       Z
-B        = (f* i+z+z+b+u+z+z) *(i**i*i-_)/i-i**i
-  U      = (f**i+z+z+b+u+z+z) +f*i-i-_
+B        = (f* i+z+z+b+u+z+z) *(i**i*i-i**z)/i-i**i
+  U      = (f**i+z+z+b+u+z+z) +f*i-i-i**z
     Z
       Z
-
 ((([[f,i,z,z,b,u,z,z].inject(&:*)]*i).inject(&:**))..(f**i+z+z+b+u+z+z)).map{|fizzbuzz|
   case [F,i,z,z,B,u,z,z].inject &:*
-  when fizzbuzz %(f+f/i)  then [F,I,Z,Z,B,U,Z,Z]
-  when fizzbuzz %(i+_)    then [F,I,Z,Z]
-  when fizzbuzz %(i**i+_) then [B,U,Z,Z]
+  when fizzbuzz %(f+f/i)     then [F,I,Z,Z,B,U,Z,Z]
+  when fizzbuzz %(i+i**z)    then [F,I,Z,Z]
+  when fizzbuzz %(i**i+i**z) then [B,U,Z,Z]
   else fizzbuzz.to_s.chars.map(&:to_i).map &(f**i/i-i).method(:+)
   end.pack 'c*'
 }.map &method(:puts)
