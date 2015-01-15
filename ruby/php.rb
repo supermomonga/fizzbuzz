1d8fce6b518bf25a8a279c79d2c0d329374d10ae
supermomonga <hi@supermomonga.com>
2015-01-15 09:52:26 +0900

    map instead of each

--- ruby/php.rb
+++ ruby/php.rb
@@ -3,7 +3,7 @@ p <<PHP_VERSION
 PHP_VERSION
 ;print "\033[1F\033[1M";
 //.tap{ define_method :range, -> s,e { s.upto e } }
-//.tap{ define_method :array_map, -> f,seq { seq.each{ |x| f.call(x) } } }
+//.tap{ define_method :array_map, -> f,seq { seq.map{ |x| f.call(x) } } }
 //.tap{ define_method :function, -> x,&b { -> x { $x=x;b.call } } }
 array_map(function($x){
   print $x % 15 == 0 ? 'FizzBuzz' : ($x % 3 == 0 ? 'Fizz' : ($x % 5 == 0 ? 'Buzz' : $x));
