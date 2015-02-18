b49800fa705d89ab0da639ee9455764c498d6963
supermomonga <hi@supermomonga.com>
2015-02-18 13:20:23 +0900

    rename

---
+++ ruby/from_nothing.rb
@@ -0,0 +1,23 @@
+
+
+# for (_.to_i).tap{|_|
+#   __ = _**_
+#   _.upto([__,__,_,_,__,_,_].join.to_i(__+__)).map{|_|_}.map(&method(:puts))
+#   } in [_]
+#   end if !_
+
+
+# for _ in [_<=>_].tap{|(_)|
+#     _.upto([_**_,_**_,_,_,_**_,_,_].join.to_i((_**_)+(_**_))).map(&method(:puts))
+#   }
+# end if !_
+
+
+
+
+p *(1..10).each_with_object(2).chunk(&:first)
+
+
+
+
+
